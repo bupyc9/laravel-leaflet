7ca09a16ce902b847e8aec338102c06d68185802
Afanasyev Pavel <p.afanasev@graphgrail.com>
2018-09-16 20:23:33 +0300

    delete unused code

--- app/Http/Controllers/PointController.php
+++ app/Http/Controllers/PointController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Category;
 use App\Point;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
--- routes/api.php
+++ routes/api.php
@@ -13,9 +13,5 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 Route::resource('categories', 'CategoryController')->only(['index', 'store', 'update']);
 Route::resource('points', 'PointController')->only(['index', 'store']);
